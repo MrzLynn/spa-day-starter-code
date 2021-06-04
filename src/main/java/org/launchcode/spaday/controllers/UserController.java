c5201901844379d945e366bbb50332dcedd5242e
Your Name <you@example.com>
2021-06-03 20:31:01 -0500

    Finished studio class 11

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,6 +3,7 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,22 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "user")
 public class UserController {
 
-    @PostMapping("/add")
+    @GetMapping("/add")
     public String displayAddUserForm(){
         return ("user/add");
     }
 
-    @RequestMapping
+    @PostMapping
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
-        if(user.getPassword() == verify) {
             model.addAttribute("user", user);
-            return ("user/index");
-        }
-        else
-            model.addAttribute("error","Wrong password, try again.");
-            model.addAttribute("Username", user.getUsername());
-            model.addAttribute("Email",user.getEmail());
-            return ("user/add");
+            model.addAttribute("verify", verify);
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            if(user.getPassword().equals(verify)){
+                return "user/index";
+            }
+            else
+            model.addAttribute("error","Wrong password, try again!");
+                return "user/add";
     }
 
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -5,12 +5,15 @@ public class User {
     private String email;
     private String password;
 
+    public User(){
+
+    }
+
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
-
 
     public String getUsername() {
         return username;
